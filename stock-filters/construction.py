17036092c3449b780dcf929b4cea31b9a9b62fc6
R Fritsch <leafar1@gmx.net>
2018-03-09 16:24:14 +0100

    abstraction for plot building

---
+++ stock-filters/construction.py
@@ -0,0 +1,164 @@
+import numpy as np
+
+from myglobals import *
+import boxutils as bu
+
+class Builder:
+
+    def __init__(self, askInterest, awardPlot, build):
+        self._askInterest = askInterest
+        self._awardPlot = awardPlot
+        self._build = build
+
+    def askInterest(self, plot):
+        'Return a value in [0., 1.] to indicate this builders interest in the plot.'
+        return self._askInterest(plot)
+
+    def awardPlot(self, plot):
+        '''
+        Informs the builder, that it is assigned this plot.
+        The builder can use this to store information with the plot.
+        '''
+        self._awardPlot(plot)
+
+    def build(self, plot):
+        'Actually place blocks on the plot.'
+        self._build(plot)
+
+########################################################################
+
+class BuilderCollective(Builder):
+
+    def __init__(self, *builders):
+        self.builders = builders
+
+    def askInterest(self, plot):
+        return max( b.askInterest(plot) for b in self.builders )
+
+    def awardPlot(self, plot):
+        weights = np.array( [ b.askInterest(plot) for b in self.builders ], dtype=float )
+        if sum(weights) > 0:
+            builder = np.random.choice( self.builders, p = (weights / sum(weights)) )
+            plot.builder = builder
+            builder.awardPlot(plot)
+
+########################################################################
+
+_registeredBuilders = []
+
+def register(builder):
+    _registeredBuilders.append(builder)
+
+def bidAndBuild(site):
+    builders = BuilderCollective(*_registeredBuilders)
+    for plot in site.plots:
+        if builders.askInterest(plot):
+            builders.awardPlot(plot)
+
+    for plot in site.plots:
+        if hasattr(plot, 'builder'):
+            plot.builder.build(plot)
+
+########################################################################
+
+def requires(plotPredicate):
+    def decorator(askInterest):
+        def inner(plot):
+            if plotPredicate(plot):
+                return askInterest(plot)
+            else:
+                return 0
+        return inner
+    return decorator
+
+notARoad = lambda plot: 'road' not in plot.tags and 'gap' not in plot.tags
+
+def minimumDimensions(minDim):
+    return lambda plot: minDim <= min(plot.width, plot.length)
+
+def maximumDimensions(maxDim):
+    return lambda plot: maxDim >= max(plot.width, plot.length)
+
+def minimumArea(area):
+    return lambda plot: area <= plot.area
+
+def maximumArea(area):
+    return lambda plot: area >= plot.area
+
+def one(plot):
+    return 1
+
+def noop(plot):
+    pass
+
+########################################################################
+
+def roadIF(plot):
+    return 1 if 'road' in plot.tags else 0
+
+def buildRoad(plot):
+    level = plot.site.level
+    if plot.width < plot.length:
+        plot = plot.expand(-1, 0, 0)
+    else:
+        plot = plot.expand(0, 0, -1)
+
+    for ground in level.groundPositions(plot):
+        level.setMaterialAt(ground, materials['Granite'])
+
+register( Builder(roadIF, noop, buildRoad) )
+
+########################################################################
+
+@requires(notARoad)
+@requires(minimumDimensions(5))
+def houseIF(plot):
+    return 1
+
+def markHouse(plot):
+    plot.tags.append('house')
+
+def buildHouse(plot):
+    MAT_WALLS = materials["Spruce Wood Planks"]
+
+    level = plot.site.level
+    level.fill(bu.ceiling(plot), MAT_WALLS)
+    level.fill(bu.floor(plot), MAT_WALLS)
+    for wall in bu.walls(plot):
+        level.fill(wall, MAT_WALLS)
+        placeWindows(level, wall)
+
+    placeDoor( level, Vector(plot.minx, plot.miny+1, (plot.minz + plot.maxz)/2) )
+
+register( Builder(houseIF, markHouse, buildHouse) )
+
+########################################################################
+
+DOOR_LOWER_DATA = {
+    Direction.East  : 0,
+    Direction.South : 1,
+    Direction.West  : 2,
+    Direction.North : 3
+}
+
+def placeDoor(level, position, direction = Direction.East, materialId = materials.blocksMatching("Oak Door")[0].ID):
+    level.setMaterialAt(position, (materialId, DOOR_LOWER_DATA[direction]))
+    level.setMaterialAt(position+Direction.Up, (materialId, 8)) # 8 => upper door with unpowered left hinge
+
+def placeWindows(level, wall):
+    MAT_WINDOWS = materials["Glass"]
+
+    wall2D = bu.BoundingBox2D(wall)
+    wall2D = wall2D.expand(-1, -1) # remove corners, floor and ceiling
+
+    if wall2D.width < 2 or wall2D.height < 2:
+        return
+
+    for x in range(0, wall2D.width-1, 3):
+            level.setMaterialAt(wall2D[x, 1], MAT_WINDOWS)
+            level.setMaterialAt(wall2D[x+1, 1], MAT_WINDOWS)
+
+    if wall2D.height >= 4:
+        for x in range(0, wall2D.width-1, 3):
+            level.setMaterialAt(wall2D[x, 2], MAT_WINDOWS)
+            level.setMaterialAt(wall2D[x+1, 2], MAT_WINDOWS)
--- stock-filters/gdmc.py
+++ stock-filters/gdmc.py
@@ -7,66 +7,18 @@ from myglobals import *
 import boxutils as bu
 from level_extensions import inject as LVinject
 from buildsite import *
+from construction import bidAndBuild
 
 # name to show in filter list
 displayName = "Settlement Generator"
 
-OPT_Material = "Material"
-
 inputs = (
 	(displayName, "label"),
-	(OPT_Material, materials.Cobblestone),
 	)
-
-MAT_WALLS = materials["Spruce Wood Planks"]
-MAT_WINDOWS = materials["Glass"]
 
 def perform(level, box, options):
     LVinject(level)
 
     site = Site(level, box)
 
-    for plot in filterByTag(site.plots, 'buildable'):
-        buildHouse(level, plot)
-
-########################################################################
-
-def buildHouse(level, box):
-    level.fill(bu.ceiling(box), MAT_WALLS)
-    level.fill(bu.floor(box), MAT_WALLS)
-    for wall in bu.walls(box):
-        level.fill(wall, MAT_WALLS)
-        placeWindows(level, wall)
-
-    placeDoor( level, Vector(box.minx, box.miny+1, (box.minz + box.maxz)/2) )
-
-########################################################################
-
-DOOR_LOWER_DATA = {
-    Direction.East  : 0,
-    Direction.South : 1,
-    Direction.West  : 2,
-    Direction.North : 3
-}
-
-#DOOR_UPPER_DATA = [8, 9] # upper unpowered L/R hinge
-
-def placeDoor(level, position, direction = Direction.East, materialId = materials.blocksMatching("Oak Door")[0].ID):
-    level.setMaterialAt(position, (materialId, DOOR_LOWER_DATA[direction]))
-    level.setMaterialAt(position+Direction.Up, (materialId, 8))
-
-def placeWindows(level, wall):
-    wall2D = bu.BoundingBox2D(wall)
-    wall2D = wall2D.expand(-1, -1) # remove corners, floor and ceiling
-
-    if wall2D.width < 2 or wall2D.height < 2:
-        return
-
-    for x in range(0, wall2D.width-1, 3):
-            level.setMaterialAt(wall2D[x, 1], MAT_WINDOWS)
-            level.setMaterialAt(wall2D[x+1, 1], MAT_WINDOWS)
-
-    if wall2D.height >= 4:
-        for x in range(0, wall2D.width-1, 3):
-            level.setMaterialAt(wall2D[x, 2], MAT_WINDOWS)
-            level.setMaterialAt(wall2D[x+1, 2], MAT_WINDOWS)
+    bidAndBuild(site)
